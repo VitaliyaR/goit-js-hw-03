cafc17f600836272296f56d17ae2b8507ccf7361
Vitaliia_Rylach <vita.elmura@gmai.com>
2025-03-23 12:45:13 +0100

    fixbags

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,19 @@
 function filterArray(numbers, value) {
     let newArray =[]; // можно використати const заміть let
-   for (let i=0; i < numbers; i++) {
+   for (let i=0; i < numbers.length; i++) {
     if ( numbers[i] > value) {
         newArray.push(numbers[i]);
 
     }
    }
+   return newArray;
 }
 
 
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
